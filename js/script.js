d02381836f2210d284751cf1200e1fe98e4d36d1
RobertoJSJTV <Robertojsj18@gmail.com>
2026-02-15 15:32:57 -0300

    updt: versão em ingles, com algumas mudanças

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function textoIngles(){
+
+    document.querySelector('.nav-link.about').textContent = 'About';
+    document.querySelector('.nav-link.history').textContent = 'History';
+
+    document.querySelector('.subtitle').textContent = 'AGENCY';
+
+
+    document.querySelector('.textoMarca h2').textContent = 'Connecting brands and creators with strategy and purpose.';
+    document.querySelector('.textoMarca p').textContent = 'We specialize in organic partnerships within the Creator Economy, prioritizing transparent negotiations and results that respect the identity of each profile.';
+    document.querySelector('.contato h4').textContent = 'Contact';
+    document.querySelector('.contato-subtitle').textContent = 'If you want to know more about my work, feel free to contact me through the channels below.';
+    document.querySelector('.p1').textContent = 'follow me on social media';
+    document.querySelector('.titulo2').textContent = 'Hello, I am Roberto Junior';
+    document.querySelector('.texto1').textContent = 'i\'m a talent agent and I founded Nexa Prime in 2022. With 15 years of experience in the gaming industry, my background blends technical expertise with the experience of someone who started creating content on YouTube back in 2011 and became a Twitch partner. Having been part of this ecosystem from the beginning, I\'ve witnessed every change firsthand. I make a point of working side-by-side with the talent I represent, prioritizing transparent and constant communication so we can grow together.';
+    document.querySelector('.subt1').textContent = 'experience';
+    document.querySelector('.subtextos1').textContent = '15 years';
+    document.querySelector('.subt2').textContent = 'Specialties';
+    document.querySelector('.subtextos2').textContent = 'Career plan 4 creators, pricing and more';
+    document.querySelector('.subt3').textContent = 'Creators';
+    document.querySelector('.subtextos3').textContent = '6 creators';
+    document.querySelector('.text-agen1').textContent = 'Agency services conducted by experienced professionals,\nwho deeply understand\nthe reality and challenges of content creators. We focus on strategic, well-structured, and responsibly executed partnerships. We also develop a personalized career plan, aiming for solid growth and long-term positioning for creators.';
+
+    document.querySelector('.btn1').textContent = 'CONTACT ME';
+
+    document.querySelector('.text-end').textContent = '© 2024 Nexa Prime. All rights reserved.';
+}
